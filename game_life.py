09257d0269c504433dae308b332d97a4a27c0bc9
AnastasiiaMurzina <hloshkin@mail.ru>
2017-05-11 07:36:48 +0400

    Update game_life.py

--- game_life.py
+++ game_life.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import sys
-
+pause = False
 f=open(sys.argv[1],'r') #первым параметром считываем с консоли файл с начальным распределением
 total_time = int(sys.argv[2]) #втором - время игры
 color='summer' #цвет по-умолчанию
@@ -52,11 +52,16 @@ def update(data):
   ttl.set_text('время ='+str(n)) #надпись со временем
   cont_life.set_text('в живых ='+str(np.count_nonzero(grid == ON))) #выводим кол-во живых
   n+=1 #увеличиваем счётчик
-  if n==total_time:
+  if n==total_time or pause==True:
       ani.event_source.stop()
   return [mat], ttl, cont_life
+
+def onClick(event):
+    global pause
+    pause ^= True
 #настраиваем анимацию
 fig, ax = plt.subplots()
+fig.canvas.mpl_connect('button_press_event', onClick) #связываем нашу анимацию и событие остановки пользователем по клику
 mat = ax.matshow(grid, cmap=(color))
 ttl = ax.text(-0.3, 1., 'время', transform = ax.transAxes)
 cont_life = ax.text(-0.3, 0., 'в живых ', transform = ax.transAxes)
@@ -65,5 +70,5 @@ cont_life.set_text('')
 ani = animation.FuncAnimation(fig, update, interval=50,repeat = False)
 #не выполнены условия остановки анимации, поэтому при остановке появляется ошибка AttributeError
 #сторчка ниже - попытка сохранить запись, но у меня не установлен ffmpeg... так что без понятия как это будет работать
-# ani.save('life.mp4', writer='ffmpeg', fps=2, frames = total_time)
+# ani.save('life.mp4', writer='ffmpeg', fps=2)
 plt.show()
